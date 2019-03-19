04ba0b0ea7c5095daf997c172f96b141b3f97b3e
gaoletian <gaoletian@finupgroup.com>
2019-03-19 14:46:06 +0800

    feat: add cookie-universal-nuxt package

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,23 +1,8 @@
 <template>
-  <v-app :dark="dark" :class="'custom--theme'">
+  <v-app :dark="$store.state.theme.dark" :class="'custom--theme'">
     <v-container>
       <nuxt/>
     </v-container>
   </v-app>
 </template>
-
-<script lang="ts">
-import { Component, Vue, namespace } from 'nuxt-property-decorator';
-import { mixins } from 'vue-class-component';
-
-const mod = namespace('theme');
-
-@Component({
-  // layout: 'default'
-})
-export default class extends Vue {
-  @mod.State
-  dark;
-}
-</script>
 
