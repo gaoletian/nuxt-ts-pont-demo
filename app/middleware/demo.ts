03681c503f1e70211132ba614415d23ed5e04dd8
gaoletian <gaoletian@finupgroup.com>
2019-07-04 16:57:57 +0800

    chore: update local snipets

--- app/middleware/demo.ts
+++
@@ -1,5 +0,0 @@
-// enable middleware you must setup pulgins at nuxt.config.js
-export default <Nuxt.Plugin>async function(ctx, inject) {
-  let pet = await ctx.$api.petstore.pet.getPetById.request({ petId: 1 });
-  console.log(pet.name);
-};
